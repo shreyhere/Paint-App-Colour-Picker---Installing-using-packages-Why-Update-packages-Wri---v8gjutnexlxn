2fcf39da62d0d73ac492319cd805761c61f5a213
shreyhere <116891326+shreyhere@users.noreply.github.com>
2022-12-16 13:28:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,24 @@
-import React, { useState } from "react";
-import '../styles/App.css';
-import Selection from './Selection';
-import ColourSelector from './ColourSelector';
-
-const colourConfig = [{
-    key: 'blue',
-    label: 'Blue',
-    classname: 'btn-blue',
-    background: 'rgb(34, 193, 195)'
-  }, {
-    key: 'orange',
-    label: 'Orange',
-    classname: 'btn-orange',
-    background: 'rgb(221, 112, 18)'
-  }, {
-    key: 'green',
-    label: 'Green',
-    classname: 'btn-green',
-    background: 'rgb(44, 209, 88)'
-  }
-]
-
-const title = 'Select the gradient and then the Box to change the color';
+import React, { useEffect, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
-  let [nextBackground, selectNextBackground] = useState({ background: "" })
-  const applyColor = (updateSelectionStyle) => {
-    updateSelectionStyle(nextBackground)
-  }
+  const [count, setCount] = useState(0);
 
   return (
-    <div id="master">
-      <h5 className="heading">{/* display title here */}</h5>
-
-      <div className="row">
-        {colourConfig.map((config, index) => (
-          <ColourSelector key={config.key} config={config} selectNextBackground={selectNextBackground} />
-        ))}
-      </div>
-
-      <div className='row' id="children-wrapper">
-        {
-          ["selection1", "selection2", "selection3"].map(key => (
-            <Selection key={key} applyColor={applyColor} />
-          ))
-        }
-      </div>
-    </div >
-  )
-}
-
+    <div className="ball">
+      <h1
+        className="count"
+        onDoubleClick={() => {
+          alert("cant edit it");
+        }}
+      >
+        {count}
+      </h1>
+      <button className="increment-button" onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+    </div>
+  );
+};
 
 export default App;
